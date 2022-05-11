b975fa2899970acdf15d286acbdf3eb3b8a0437f
Beck <guille_12.12@hotmail.com>
2022-05-11 14:18:15 -0300

    primer comm

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React          from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.jsx';
+import './index.css';
+
+const divRoot = document.querySelector('#root')
+const root = createRoot(divRoot); // createRoot(container!) if you use TypeScript
+
+root.render(<App />)
+
